6c62a959257c6a45e9104111bd636ea2235559ba
githubProjectsgth <githubProjectsgth@gmail.com>
2023-07-07 14:11:27 -0700

    ..

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BookingPage from './components/BookingPage';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+
+    <>
+      <Nav></Nav>
+    </>
+  );
+}
+
+export default App;
